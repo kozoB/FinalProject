32c7df886f4ad8e23b0c4d99d4b00a67723257bc
Shoval Ashkenazi <shovalash1@gmail.com>
2022-10-22 17:26:10 +0300

    update

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -11,15 +11,15 @@ const mapController = require ('../controllers/mapController')
 
 //  app Routes
 
-router.post('/', recipeController.homepage);
-router.post('/recipe/:id', recipeController.exploreRecipe );
-router.post('/categories', recipeController.exploreCategories);
-router.post('/categories/:id', recipeController.exploreCategoriesById);
+router.get('/', recipeController.homepage);
+router.get('/recipe/:id', recipeController.exploreRecipe );
+router.get('/categories', recipeController.exploreCategories);
+router.get('/categories/:id', recipeController.exploreCategoriesById);
 
 
- router.post('/login' , loginController.LoginPage);
- router.post('/register' , registerController.RegisterPage);
- router.post('/map' , mapController.MapPage); /////////////////////////// BAR - Added map page route
+ router.get('/login' , loginController.LoginPage);
+ router.get('/register' , registerController.RegisterPage);
+ router.get('/map' , mapController.MapPage); /////////////////////////// BAR - Added map page route
  
  router.post('/signup' , signupController.signup);
     
