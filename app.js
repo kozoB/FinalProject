9799affc5831fcd6e672f89f0dd2fa140557941c
Nadav <a15nadav@gmail.com>
2022-11-15 12:49:00 +0200

    path

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+const path=require('path');
+require('dotenv').config({path:path.resolve(__dirname, '../.env')})
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const session = require("express-session");
